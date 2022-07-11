b3e78accea4f26c646e3c8e51077845889800c1c
seokHan <sseok1979@naver.com>
2022-07-11 18:15:00 +0900

    router setting

---
+++ src/routes/index.js
@@ -0,0 +1,19 @@
+import { createRouter, createWebHashHistory } from "vue-router";
+import Home from './Home'
+import About from './About'
+
+export default createRouter({
+    // Hash -> https://google.com/#/search
+    history: createWebHashHistory(),
+    // pages
+    routes: [
+      {
+        path: '/',
+        component: Home
+      },
+      {
+        path: '/about',
+        component: About
+      }
+    ]
+})
