90fdc4822c4b203cc9314c62b250682befdb003f
seokHan <sseok1979@naver.com>
2022-07-11 17:16:07 +0900

    vue3 toyProject

---
+++ .eslintrc.js
@@ -0,0 +1,46 @@
+module.exports = {
+    parser: 'vue-eslint-parser',
+    env: {
+        browser: true,
+        node: true
+    },
+    extends: [
+        // vue
+        //'plugin:vue/vue3-essential', // Lv1
+        'plugin:vue/vue3-strongly-recommended', // Lv2
+        //'plugin:vue/vue3-recommended', // Lv3
+
+        // js
+        'eslint:recommended'
+    ],
+    parserOptions: {
+        
+    },
+    rules: {
+        "vue/html-closing-bracket-newline": ["error", {
+            "singleline": "never",
+            "multiline": "never"
+        }],
+        "vue/html-self-closing": ["error", {
+            "html": {
+                "void": "always",
+                "normal": "never",
+                "component": "always"
+            },
+            "svg": "always",
+            "math": "always"
+        }],
+        "vue/script-indent": ["error", 2, { 
+            "baseIndent": 0 
+        }],
+        'vue/multi-word-component-names': 'off'
+    },
+    "overrides": [
+        {
+          "files": ["*.vue"],
+          "rules": {
+            "indent": "off"
+          }
+        }
+      ]   
+}
---
+++ src/App.vue
@@ -0,0 +1,40 @@
+<template>
+  <!-- 해당 component에 slot으로 전달 
+  root Element가 2개이상이면 class와 style로 상속이 안됨 -->
+  <MyBtn>Banana</MyBtn>
+  <MyBtn :color="color">
+    Apple
+  </MyBtn>
+  <MyBtn
+    large
+    color="royalblue">
+    Cherry
+  </MyBtn>
+  <MyBtn>strawberry</MyBtn>
+
+  <!-- component - emit 
+  내가 원하는 이름으로 설정 해도 됨-->
+  <MyBtn @sseok="log">
+    grapes
+  </MyBtn>
+</template>
+
+<script>
+import MyBtn from '~/components/MyBtn'
+
+export default {
+  components: {
+    MyBtn
+  },
+  data() {
+    return {
+      color: '#000'
+    }
+  },
+  methods:{
+    log(){
+      console.log('click graeps')  
+    }
+  }
+}
+</script>
